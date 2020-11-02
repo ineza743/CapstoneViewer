49077aa129f8f77e19b8df4a8a165c6eeebdc567
ineza743 <afsanat.ineza@ashesi.edu.gh>
2020-11-02 10:50:51 +0000

    adding all files

---
+++ Admin-signup.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SignUp</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link rel="stylesheet" href="index.css">
+</head>
+
+<body style= "background-color: burlywood;">
+	
+<div class="container-fluid">
+		<div class="container">
+			<h2 class="text-center" id="title">Sign up</h2>
+ 	
+			<div class="text-center" class="row">
+				<div class="text-center" class="col-md-5">
+ 					<form  role="form" method="post" action="index.html">
+												
+ 							<div class="form-group">
+							
+								<input type="text" name="username" id="username" class="form-control input-lg" placeholder="username">
+							</div>
+
+							<div class="form-group">
+							
+								<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address">
+							</div>
+
+							<div class="form-group">
+							
+							<input type="number" name="ID" id="ID" class="form-control input-lg" placeholder="Administrator ID">
+						</div>
+
+							<div class="form-group">
+								<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password">
+							</div>
+
+		
+ 							<div>
+ 									  <input type="submit" class="btn btn-lg btn-primary"   value="SignUp">
+ 							</div>
+						
+					</form>
+				</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<!--Footer section-->
+<section id="footer">
+
+    <div class="footer">
+        <p> &copy; CapstoneViewer 2020 | Powered by Afsanat </p>
+    </div>
+
+</section>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+
---
+++ business.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> CapstoneViewer </title>
+
+    
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+    <!-- my external css-->
+    <link rel="stylesheet" href="business.css">
+
+</head>
+<body>
+
+
+<!-- HEADER (NAVBAR) -->
+<section id="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+                <a class="navbar-brand" ><img src="logo.JPG" height="90px" width="150px"></a>
+               
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item ">
+                      <a class="nav-link active" href="#">Home </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#banner">About </a>
+                      </li>
+                      <li class="nav-item ">
+                        <a class="nav-link" href="#">Statistics </a>
+                      </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#action-btn">Logout </a>
+                    </li>
+                  </ul>
+        </div>
+        </div> 
+      </nav>
+</section>
+
+
+
+
+<!--BA-->
+<div class="row">
+
+
+  <div class="col-md-6 mb-4">
+
+    <div class="card example-1 scrollbar-ripe-malinka">
+      <div class="card-body">
+        <h4 id="section1"><strong>Business administration Major</strong></h4>
+        <a href="#">01/10/2020    weather application</a><br>
+        <a href="#">02/10/2020    Cyusa More</a><br>
+        <a href="#">03/10/2020    Meme generator</a><br>
+        <a href="#">04/10/2020    Google map</a><br>
+        <a href="#">26/10/2020    Library website</a><br>
+        <a href="#">27/10/2020    Blog</a><br>
+        <a href="#">28/10/2020    Uwase Delilah</a><br>
+        <a href="#">29/10/2020    Uwase Delilah</a><br>
+        <a href="#">30/10/2020    Uwase Delilah</a><br>
+      </div>
+    </div>
+
+  </div>
+
+
+
+<!--Footer section-->
+<section id="footer">
+
+    <div class="footer">
+        <p> &copy; CapstoneViewer 2020 | Powered by Afsanat </p>
+    </div>
+
+</section>
+
+    <!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
